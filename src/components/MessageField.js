10f55ed359c89be926186a02b9aa97000fae66ca
Kraeva AV <kraeva811625@gmail.com>
2021-04-11 11:21:06 +0300

    lesson3

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,32 +1,42 @@
 import React, { useState, useCallback, useEffect } from "react";
 import Message from "./Message";
 import {AUTORS} from "../utils/constans";
+import Input from "./input";
 
 const initalMessages = [
-  {sender: "bot", text: "Привет"}, 
-  {sender: "bot", text: "Как дела?"}
+  {sender: "bot", text: "Привет", id:'1'}, 
+  {sender: "bot", text: "Как дела?", id:'2'}
 ];
 
 const MessageField = () => {
   const [messages, setMessages] = useState(initalMessages);
 
-  const addMessage = useCallback(() => {
-    setMessages((prevMess) => [...prevMess, {sender: AUTORS.ME, text: "Нормально, сам как?"}]);
+  const addMessage = useCallback((newMessage) => {
+    setMessages((prevMess) => [...prevMess, {...newMessage, id: prevMess.length + 1}]);
   }, []);
 
   useEffect(() => {
-    let lastIndex = messages.length - 1;
-    console.log(AUTORS.ME)
-    if (messages[lastIndex].sender == "me") {
-      setTimeout(() => setMessages((prevMess) => [...prevMess, {sender: AUTORS.BOT, text: "У меня все отлично, я бот)"}]), 1500);
+    const lastMessage = messages[messages.length - 1];
+    let timeout;
+
+    if (lastMessage?.sender != AUTORS.BOT) {
+      timeout = setTimeout(() => {
+        addMessage({
+          sender: AUTORS.BOT, 
+          text: "У меня все отлично, я бот)",
+          id: messages.length + 1,
+      });
+      }, 1500);
     }
-  }, [messages])
+
+    return () => clearTimeout(timeout);
+  }, [messages]);
 
   return (
-    <>
-      {messages.map((item, index) => (<Message key={index} sender ={item.sender} text={item.text}/>))}
-      <button onClick={addMessage}>Отправить</button>
-    </>
+    <div>
+      {messages.map((item) => (<Message key={item.id} sender={item.sender} text={item.text} class={item.sender == AUTORS.BOT ? 'message bot_message':'message my_message'}/>))}
+      <Input onAddMessage={addMessage}/>
+    </div>
   );
 };
 
---
+++ src/components/input.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {AUTORS} from "../utils/constans";
+import {TextField, Button} from "@material-ui/core";
+
+export default function Input({onAddMessage}) {
+    const [value, setValue] = useState('');
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAddMessage({
+            sender: AUTORS.ME, 
+            text: value,
+            id: null,
+        });
+        setValue('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField value={value} onChange={handleChange}/>
+            <Button color="primary" type="submit">Отправить</Button>
+            {/* <input type="submit" /> */}
+        </form>
+    )
+}
